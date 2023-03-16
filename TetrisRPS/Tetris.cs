b5ef8ddd47f59c97c143e630ed3c1ccdd93572ec
Nikola <nikolape7@gmail.com>
2023-03-16 19:19:10 +0100

    Public functions

---
+++ TetrisRPS/Tetris.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisRPS
+{
+    internal class Tetris
+    {
+        public int[,] array = new int[10, 20];
+        public int holding;
+        public int next;
+        public int button;
+        public void ButtonPress(int butt) 
+        {
+            button = butt;
+        }
+        
+        public void Tick()
+        {
+
+        }
+    }
+}
