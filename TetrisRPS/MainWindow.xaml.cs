0494abe9d5739a45dad35e2041769558934dd427
OzzanAr <ozan.arif@student.um.si>
2023-03-18 20:48:48 +0100

    Added comments explaning the layout. Added all the assets for the tiles and blocks. Created arrays to access the arrays and blocks.

--- TetrisRPS/MainWindow.xaml.cs
+++ TetrisRPS/MainWindow.xaml.cs
@@ -20,10 +20,78 @@ namespace TetrisRPS
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Array containg the tile images
+        // Order goes first with the empty tile then corresponds to each blocks ID
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileRed.png", UriKind.Relative))
+        };
+
+        // Array contains full picture of the blocks
+        // Used in holding and upcoming block 
+        // The order matches each blocks ID
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-Z.png", UriKind.Relative))
+        };
+
+        // For each of the game grid cells there is and image control
+        private readonly Image[,] imageControls;
+
+        // TODO - GameState object
+        
+
         public MainWindow()
         {
             InitializeComponent();
             //this is comment
         }
+
+        // TODO
+        // Method for setting up the image controls on the canvas
+        // The image controls array should contain 22 rows and 10 collumns (10x22)
+        
+        // TOOD
+        // Drawing the game grid
+
+        // TODO
+        // Draw the current block
+
+        // TODO 
+        // Combine the drawing of the game grid with drawing the current block so it is done at the same time
+
+        // Detecting player input
+        // Function is called inside the Window
+        private void WindowKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        // Draw the the game grid on the canvas
+        // The function is called inside the canvas using the Loaded function
+        private void GameCanvasLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Play again button
+        // The button appears in the overlay once the game is over
+        // Honestly not sure how this one is going to work with two players having to press it at the same time
+        
+
     }
 }
